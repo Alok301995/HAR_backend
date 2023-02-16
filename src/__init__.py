16931e75fc85464cdb3887f7090f1730e9a26c48
Alok Dhiman <alokdhiman@Aloks-MacBook-Air.local>
2023-02-16 21:49:04 +0530

    Index page Added

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask , render_template
 import os
 from flask_cors import CORS
 from src.routes.eval import eval
+from src.routes.home import home
 
 # Create Flask app
 
@@ -9,23 +10,7 @@ from src.routes.eval import eval
 app = Flask(__name__,instance_relative_config = True)
 CORS(app)
 
+
+app.register_blueprint(home)
 app.register_blueprint(eval)
 
-
-# def create_app(test_config = None):
-#     app = Flask(__name__,instance_relative_config = True)
-#     CORS(app)
-    
-#     # if test_config is None:
-#     #     app.config.from_mapping(
-#     #         SECRET_KEY = os.environ.get('SECRET_KEY')
-#     #     )
-#     # else:
-#     #     app.config.from_mapping(test_config)
-        
-        
-#     # Here we will register our blueprint Routes
-#     app.register_blueprint(eval)
-    
-        
-#     return app
---
+++ src/routes/home.py
@@ -0,0 +1,12 @@
+from flask import Blueprint , render_template
+
+
+
+
+
+home = Blueprint("Home" ,__name__ ,url_prefix= '/')
+
+
+@home.route('' , methods = ['GET'])
+def index():
+    return render_template('index.html')
